a681bedaed4da5a1aaf8aa5c5e9df7c6e66cf3e4
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-06-03 09:53:28 +0500

    add menu button

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram import F
 from config import BOT_TOKEN,ADMINS
+from keyboard_buttons import menu_button
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
@@ -12,7 +13,7 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     full_name = message.from_user.full_name
-    await message.answer(f"Salom, {full_name}")
+    await message.answer(f"Salom, {full_name}",reply_markup=menu_button)
 
 
 async def main():
